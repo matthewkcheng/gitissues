ee28fc49a91154125110d232c615d9068519709b
Matt Cheng <mattcheng@matts-mbp-3.home>
2020-08-10 12:00:06 -0700

    Updates

---
+++ precache-manifest.9bdf4dec0ab8d1dd09ad40aef2d5a96b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16c84874277bb297c4eeace3286a379",
+    "url": "/gitissues/index.html"
+  },
+  {
+    "revision": "ee05171d59f21ec33122",
+    "url": "/gitissues/static/js/2.1366fa59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gitissues/static/js/2.1366fa59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d9e184c0f4289265f09",
+    "url": "/gitissues/static/js/main.dcf5de32.chunk.js"
+  },
+  {
+    "revision": "c1951c90c578afb2819f",
+    "url": "/gitissues/static/js/runtime-main.2bcf4522.js"
+  }
+]);
